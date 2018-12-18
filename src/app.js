12d5ed8fde4b60c9bd0e412230952420ee832239
Przemo400 <przemek@MacBook-Pro.localdomain>
2018-12-18 21:40:42 +0100

    ddd

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const store = configureStore(); 
+
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000)
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+  <div>
+
+  <Provider store={store}>
+
+      <AppRouter />
+  </Provider>
+  </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
